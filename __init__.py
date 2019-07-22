931fe22f3bd231ce4bb13ac6559a7475fd039711
bitranox <rnowotny1966@gmail.com>
2019-07-22 20:49:56 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -2,8 +2,3 @@
 
 # this __init__.py is only meant for local package development
 from .lib_regexp import *
-
-__title__ = 'lib_regexp'
-__version__ = '1.0.0'
-__name__ = 'lib_regexp'
-
--- lib_regexp/__init__.py
+++ lib_regexp/__init__.py
@@ -3,5 +3,5 @@
 from .lib_regexp import *
 
 __title__ = 'lib_regexp'
-__version__ = '1.0.0'
+__version__ = '0.0.1'
 __name__ = 'lib_regexp'
