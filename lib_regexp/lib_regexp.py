b8e5f35ecfd1e148be9c30f427cdb3116e9e66af
bitranox <1uGToNSiYo85pWhQcqFu>
2019-07-25 11:13:01 +0200

    refractor

--- lib_regexp/lib_regexp.py
+++ lib_regexp/lib_regexp.py
@@ -129,7 +129,7 @@ regexp_check_chars_not_az09pointdash = ClassRegexExecute('[^a-z0-9.-]')        #
 regexp_non_standard_unicode_characters = ClassRegexExecute(r'[^\w\s\^°!"§$%&/\'\(\)\[\]{}\~€@\+\-\*\|\=\?\>\<,;\.:#`²³©®¼½¾ª™øØ\\]', flags=re.UNICODE)
 
 
-def reg_grep(pattern, text, flags=re.MULTILINE | re.UNICODE, pattern_regexp=False):
+def reg_grep(pattern, text, flags=re.MULTILINE | re.UNICODE, pattern_is_regexp=False):
     # type: (str, str, Union[int, re.RegexFlag], bool) -> List[str]
     # old  style type annotation because PEP8 E251 around flags: Union[int, re.RegexFlag] = re.MULTILINE | re.UNICODE
 
@@ -139,11 +139,11 @@ def reg_grep(pattern, text, flags=re.MULTILINE | re.UNICODE, pattern_regexp=Fals
     # https://www.guru99.com/python-regular-expressions-complete-tutorial.html#5
 
     >>> assert reg_grep('a', 'this is a multiline\\nstring with some\\nmatching lines') == ['this is a multiline', 'matching lines']
-    >>> assert reg_grep(r'^.*a.*$', 'this is a multiline\\nstring with some\\nmatching lines', pattern_regexp=True) == ['this is a multiline', 'matching lines']
+    >>> assert reg_grep(r'^.*a.*$', 'this is a multiline\\nstring with some\\nmatching lines', pattern_is_regexp=True) == ['this is a multiline', 'matching lines']
 
 
     """
-    if pattern_regexp:
+    if pattern_is_regexp:
         regexp_pattern = pattern
     else:
         regexp_pattern = r"^.*{pattern}.*$".format(pattern=pattern)
@@ -152,7 +152,7 @@ def reg_grep(pattern, text, flags=re.MULTILINE | re.UNICODE, pattern_regexp=Fals
     return l_results
 
 
-def reg_is_str_in_text(pattern, text, flags=re.MULTILINE | re.UNICODE, pattern_regexp=False):
+def reg_is_str_in_text(pattern, text, flags=re.MULTILINE | re.UNICODE, pattern_is_regexp=False):
     # type: (str, str, Union[int, re.RegexFlag], bool) -> bool
     # old  style type annotation because PEP8 E251 around flags: Union[int, re.RegexFlag] = re.MULTILINE | re.UNICODE
 
@@ -164,14 +164,14 @@ def reg_is_str_in_text(pattern, text, flags=re.MULTILINE | re.UNICODE, pattern_r
     >>> assert reg_is_str_in_text('a', 'this is a multiline\\nstring with some\\nmatching lines') == True
     >>> assert reg_is_str_in_text('matching', 'this is a multiline\\nstring with some\\nmatching lines') == True
     >>> assert reg_is_str_in_text('z', 'this is a multiline\\nstring with some\\nmatching lines') == False
-    >>> assert reg_is_str_in_text(r"^.*a.*$", 'this is a multiline\\nstring with some\\nmatching lines', pattern_regexp=True) == True
-    >>> assert reg_is_str_in_text(r"^.*matching.*$", 'this is a multiline\\nstring with some\\nmatching lines', pattern_regexp=True) == True
-    >>> assert reg_is_str_in_text(r"^.*z.*$", 'this is a multiline\\nstring with some\\nmatching lines', pattern_regexp=True) == False
+    >>> assert reg_is_str_in_text(r"^.*a.*$", 'this is a multiline\\nstring with some\\nmatching lines', pattern_is_regexp=True) == True
+    >>> assert reg_is_str_in_text(r"^.*matching.*$", 'this is a multiline\\nstring with some\\nmatching lines', pattern_is_regexp=True) == True
+    >>> assert reg_is_str_in_text(r"^.*z.*$", 'this is a multiline\\nstring with some\\nmatching lines', pattern_is_regexp=True) == False
 
     """
     # line = re.search(r"^.*HEAD.*$", result.stdout, re.MULTILINE)
 
-    if pattern_regexp:
+    if pattern_is_regexp:
         regexp_pattern = pattern
     else:
         regexp_pattern = r"^.*{pattern}.*$".format(pattern=pattern)
