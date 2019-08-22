8a35fcf0e4204287baae25fdf64246145a343979
bitranox <rnowotny1966@gmail.com>
2019-08-22 20:12:03 +0200

    refractor

--- conftest.py
+++ conftest.py
@@ -7,12 +7,13 @@ collect_ignore = ['build_docs.py']
 
 def pytest_cmdline_preparse(args):
     # run tests on multiple processes if pytest-xdist plugin is available
+    """
     if "xdist" in sys.modules:  # pytest-xdist plugin
         import multiprocessing
 
         num = int(max(multiprocessing.cpu_count() / 2, 1))
         args[:] = ["-n", str(num)] + args
-
+    """
     # add mypy option if not pypy
     if platform.python_implementation() != "PyPy":
         args[:] = ["--mypy"] + args
