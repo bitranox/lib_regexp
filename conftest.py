12ba004c8f2a2ee3bb430fb625c809d58a23d0c0
bitranox <rnowotny1966@gmail.com>
2019-08-22 20:25:41 +0200

    refractor

--- conftest.py
+++ conftest.py
@@ -1,19 +1,20 @@
 import platform
-import sys
 
-# collect_ignore = ['build_docs.py', 'setup.py', 'dist.py']
 collect_ignore = ['build_docs.py']
 
 
 def pytest_cmdline_preparse(args):
-    # run tests on multiple processes if pytest-xdist plugin is available
     """
+    # run tests on multiple processes if pytest-xdist plugin is available
+    # unfortunately it does not work with codecov
+    import sys
     if "xdist" in sys.modules:  # pytest-xdist plugin
         import multiprocessing
 
         num = int(max(multiprocessing.cpu_count() / 2, 1))
         args[:] = ["-n", str(num)] + args
     """
+
     # add mypy option if not pypy
     if platform.python_implementation() != "PyPy":
         args[:] = ["--mypy"] + args
