eb3c9f1b94f9f6404274ca7d393e17d3dd629098
bitranox <rnowotny1966@gmail.com>
2019-08-22 19:04:50 +0200

    refractor

--- conftest.py
+++ conftest.py
@@ -1,2 +1,13 @@
+import sys
+
 # collect_ignore = ['build_docs.py', 'setup.py', 'dist.py']
 collect_ignore = ['build_docs.py']
+
+
+def pytest_load_initial_conftests(args):
+    # run tests on multiple processes if pytest-xdist plugin is available
+    if "xdist" in sys.modules:  # pytest-xdist plugin
+        import multiprocessing
+
+        num = max(multiprocessing.cpu_count() / 2, 1)
+        args[:] = ["-n", str(num)] + args
