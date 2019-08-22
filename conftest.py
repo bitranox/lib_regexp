dab06e66c4e7a718858822959cc8f87945a449af
bitranox <rnowotny1966@gmail.com>
2019-08-22 19:31:03 +0200

    refractor

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def pytest_cmdline_preparse(args):
     if "xdist" in sys.modules:  # pytest-xdist plugin
         import multiprocessing
 
-        num = max(multiprocessing.cpu_count() / 2, 1)
+        num = int(max(multiprocessing.cpu_count() / 2, 1))
         args[:] = ["-n", str(num)] + args
 
     # add mypy option if not pypy
