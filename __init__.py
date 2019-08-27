a443effc9084e8d8309ad96741d4586d875440c3
bitranox <rnowotny1966@gmail.com>
2019-08-27 12:26:25 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # this __init__.py is only meant for local package development
-try:
-    from .lib_regexp import *
-except ImportError:                 # this we need for pip install --install-option test
-    import lib_regexp
+# try:
+from .lib_regexp import *
+# except ImportError:                 # this we need for pip install --install-option test
+#     import lib_regexp
