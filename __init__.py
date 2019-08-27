23690469f293c96a1aa9c573dc0d4412eab327c6
bitranox <rnowotny1966@gmail.com>
2019-08-27 12:48:42 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 # this __init__.py is only meant for local package development
 # try:
 
-from .lib_regexp import *   # mypy: ignore_missing_imports
+from .lib_regexp import *   # mypy: follow_imports=skip
 
 # except ImportError:
 #    import lib_regexp
